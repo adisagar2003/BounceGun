cb2c49eb8ca5105868338e81f0754ec07e7e31ee
Adi <adityasagarpython@gmail.com>
2025-04-25 18:07:40 -0300

    feat: separate class player bullet

--- Assets/Code/Player/Combat/PlayerBullet.cs
+++ Assets/Code/Player/Combat/PlayerBullet.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Bullet that player shoots to hit enemy
+/// Future: A base class for enemy bullet and player bullet can be made.
+/// </summary>
 public class PlayerBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float destroyBulletTime = 2.0f;
+    [SerializeField] private float bulletDamage = 13.0f;
+
+
     void Start()
     {
-        
+        StartCoroutine(DestroyBullet());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        // for bullet shot by enemy
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy Hit");
+            BaseEnemy enemyRef = other.GetComponent<BaseEnemy>();
+            if (enemyRef)
+            {
+                enemyRef.TakeDamage(bulletDamage);
+            }
+            Destroy(gameObject);
+        }
+
+    }
+    private IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
